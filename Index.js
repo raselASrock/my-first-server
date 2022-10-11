f53699521962c0fd83b5924ddb794247b3a37eee
raselASrock <mdraselm413@gmail.com>
2022-10-12 00:54:01 +0600

    data

--- Index.js
+++ Index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
+const phones = require('./phones.json')
 const port = 5000
 
 
 app.get('/', (req , res) =>{
-    res.send('hello world from node')
+    res.send('hLok Mama ello world from node')
+});
+
+app.get('/phones', (req, res) => {
+    res.send(phones)
+});
+
+
+app.get('/phones/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log('Lokking for data in id:', id);
+
+    const phone = phones.find(ph => ph.id === id) || {};
+    res.send(phone)
 });
 
 
